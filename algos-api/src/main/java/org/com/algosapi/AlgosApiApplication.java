c0ffd4619cb4348f7040e9958a67a8814d6d927b
codesche <codesche@gmail.com>
2025-09-16 01:53:49 +0900

    작업내용 커밋

---
+++ algos-api/src/main/java/org/com/algosapi/AlgosApiApplication.java
@@ -0,0 +1,13 @@
+package org.com.algosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgosApiApplication.class, args);
+    }
+
+}
---
+++ algos-api/src/test/java/org/com/algosapi/AlgosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.algosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
